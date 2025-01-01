d3e414b9c7043c27d9d2a2a96d2d855a512384ed
Chhayhong <chhayhong@Chhayhongs-MacBook-Pro.local>
2025-01-01 19:31:22 +0700

    add validation to entity

--- src/users/users_entity.py
+++ src/users/users_entity.py
@@ -7,10 +7,10 @@ class Users(config.Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    username = mapped_column(String, unique=True, index=True)
-    email = mapped_column(String, unique=True)
-    password = mapped_column(String)
-    role = mapped_column(String, default="user")
+    username = mapped_column(String, unique=True, index=True,nullable=False)
+    email = mapped_column(String, unique=True,nullable=False)
+    password = mapped_column(String(255), nullable=False)
+    role = mapped_column(String(64), default="user")
     refresh_token = mapped_column(String)
 
 
