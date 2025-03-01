01e2d7393abfb0830ebd3a703eaeebf41f497bf7
Chhayhong <thatchhayhong@gmail.com>
2025-03-01 16:02:00 +0700

    Finish

--- ui/src/app/app.routes.server.ts
+++ ui/src/app/app.routes.server.ts
@@ -1,8 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  { path: 'login', renderMode: RenderMode.Client },
-  { path: 'register', renderMode: RenderMode.Client },
+  { path: 'auth/login', renderMode: RenderMode.Client },
+  { path: 'auth/register', renderMode: RenderMode.Client },
   
   // {
   //   path: 'error',
